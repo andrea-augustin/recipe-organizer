1b90d162a33f817bbb5dd572e2796e3753be1ee7
Andrea Augustin <augustin.andrea@campus.lmu.de>
2021-03-10 16:46:01 +0100

    copied file from local project and cleaned up code

---
+++ grabbers/essen_trinken_grabber.py
@@ -0,0 +1,208 @@
+from bs4 import BeautifulSoup
+import requests
+from time import sleep
+import re
+from os.path import exists
+
+url_basic = "https://www.essen-und-trinken.de"
+url_archive = "https://www.essen-und-trinken.de/rezepte/archiv"
+
+
+def save_recipe_to_file(recipe, path):
+    file_open_mode = 'a' if exists(path) else 'w'
+
+    with open(path, file_open_mode, encoding='utf-8') as f:
+        f.write("Titel\t" + recipe['title'] + "\n")
+        f.write("URL\t" + recipe['url'] + "\n")
+        f.write("Seite\t" + str(recipe['page']) + "\n")
+        f.write("Portionen\t" + str(recipe['servings']) + "\n")
+        f.write("Dauer\t" + recipe['prep_time'] + "\n")
+        if recipe['categories'] is None:
+            f.write("Kategorien\n")
+        else:
+            f.write("Kategorien\t" + '\t'.join(recipe['categories']) + "\n")
+        f.write("Zutaten\n")
+        for ingredient in recipe['ingredients']:
+            f.write(ingredient + "\n")
+        f.write("Zubereitung\n")
+        for step in recipe['steps']:
+            f.write(step + "\n")
+        f.write("\n")
+
+
+def scrap_skipped_pages():
+    with open('../../data/essen_und_trinken/new_skipped.txt', 'r') as f:
+        urls = f.readlines()
+
+    skipped = []
+
+    for recipe_url in urls:
+        recipe_url = recipe_url.strip('\n')
+        req_recipe = requests.get(recipe_url)
+        soup_recipe = BeautifulSoup(req_recipe.content, 'html.parser')
+        recipe = scrap_essen_und_trinken_recipe(soup_recipe, recipe_url)
+
+        if all(['essen_und_trinken' not in recipe['origin'], 'Für_jeden_Tag' not in recipe['origin'],
+                'essen_und_trinken' not in recipe['origin']]):
+            continue
+
+        file_path = 'essen_und_trinken/' + recipe['origin'] + '.txt'
+
+        save_recipe_to_file(recipe, file_path)
+
+        sleep(1)
+
+    if len(skipped) == 0:
+        return
+
+    with open("skipped.txt", 'a', encoding="utf-8") as f:
+        for skipped_recipe in skipped:
+            f.write(skipped_recipe + '\n')
+
+
+def scrap_essen_und_trinken_pages():
+    skipped = []
+
+    for i in range(0, 1145):
+        print("Seite " + str(i + 1) + " von 1145")
+        recipe_links = []
+        url = url_archive + '?page=' + str(i)
+        req = requests.get(url)
+        soup = BeautifulSoup(req.content, 'html.parser')
+
+        page_recipe_overview = soup.find("div", class_="panel-panel panel-col")
+        for a_element in page_recipe_overview.find_all('a', href=True):
+            if 'rzpt' in a_element['href']:
+                recipe_links.append(a_element['href'])
+
+        sleep(1.5)
+
+        for recipe_url in recipe_links:
+            req_recipe = requests.get(url_basic + recipe_url)
+            soup_recipe = BeautifulSoup(req_recipe.content, 'html.parser')
+            try:
+                recipe = scrap_essen_und_trinken_recipe(soup_recipe, url_basic + recipe_url)
+            except Exception:
+                print("Rezept übersprungen: " + url_basic + recipe_url)
+                skipped.append(url_basic + recipe_url)
+                sleep(1.5)
+                continue
+
+            if all(['essen_und_trinken' not in recipe['origin'], 'Für_jeden_Tag' not in recipe['origin'],
+                    'essen_und_trinken' not in recipe['origin']]):
+                continue
+
+            file_path = recipe['origin'] + '.txt'
+
+            save_recipe_to_file(recipe, file_path)
+
+            sleep(1.5)
+
+    if len(skipped) > 0:
+        with open("skipped.txt", 'a', encoding="utf-8") as f:
+            for skipped_recipe in skipped:
+                f.write(skipped_recipe + '\n')
+
+
+def get_origin_information_from_recipe_page(soup):
+    origin_element = soup.find("div", class_="recipe-references").find("div", class_="source-reference")
+    if origin_element:
+        return origin_element.text.split('\n')[-1].strip(' ').replace(' ', '_').replace('/', '_')
+    else:
+        return "essen_und_trinken"
+
+
+def get_servings_information_from_recipe_page(soup, ingredients_element):
+    servings_element = soup.find("div", class_="servings")
+
+    if servings_element:
+        return servings_element.text.strip()
+    else:
+        specific_serving_elements = ingredients_element.find("ul", class_="ingredients-list").find_all("li",
+                                                                                                       class_="ingredients-zwiti")
+
+        if len(specific_serving_elements) == 0:
+            return 0
+        else:
+            return specific_serving_elements[0].text
+
+
+def get_prep_time_information_from_recipe_page(soup):
+    prep_time_element = soup.find("div", class_="time-preparation")
+    if prep_time_element:
+        prep_time = soup.find("div", class_="time-preparation").text
+        additional_prep_element = soup.find_all("div", class_="time-addon")
+
+        if additional_prep_element:
+            for additional_prep in additional_prep_element:
+                prep_time += ' ' + additional_prep.text
+
+        return prep_time
+    else:
+        return "Nicht angegeben"
+
+
+def get_categories_information_from_recipe_page(soup):
+    category_page_element = soup.find("ul", class_="taxonomies-list")
+
+    if category_page_element is None:
+        return None
+    else:
+        categories_children = category_page_element.findChildren('li', recursive=False)
+        categories = []
+
+        for category_element in categories_children:
+            categories.append(category_element.text)
+
+        return categories
+
+
+def get_ingredients_information_from_recipe_page(ingredients_element):
+    ingredients = []
+    for ingredient_element in ingredients_element.find_all('li'):
+        if len(ingredient_element.attrs) > 0:
+            continue
+
+        ingredients.append(re.sub(' +', ' ', ingredient_element.text.strip()))
+
+    return ingredients
+
+
+def get_prep_steps_information_from_recipe_page(soup):
+    prep_steps = []
+    prep_steps_element = soup.find("ul", class_="preparation").findChildren("li", class_="preparation-step",
+                                                                            recursive=False)
+
+    for step in prep_steps_element:
+        prep_steps.append(step.find("div", class_="preparation-text").text)
+
+    return prep_steps
+
+
+def scrap_essen_und_trinken_recipe(soup, recipe_url):
+    ingredients_element = soup.find("section", class_="ingredients")
+    recipe = dict()
+
+    recipe['url'] = recipe_url
+
+    recipe['title'] = soup.find("span", class_="headline-title").text
+
+    recipe['origin'] = get_origin_information_from_recipe_page(soup)
+
+    recipe['page'] = 0
+
+    recipe['servings'] = get_servings_information_from_recipe_page(soup, ingredients_element)
+
+    recipe['prep_time'] = get_prep_time_information_from_recipe_page(soup)
+
+    recipe['categories'] = get_categories_information_from_recipe_page(soup)
+
+    recipe['ingredients'] = get_ingredients_information_from_recipe_page(ingredients_element)
+
+    recipe['steps'] = get_prep_steps_information_from_recipe_page(soup)
+
+    return recipe
+
+
+if __name__ == '__main__':
+    scrap_skipped_pages()
